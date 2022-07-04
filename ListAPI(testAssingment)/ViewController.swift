74320cb3520566e5200357faac5b3ab4f66afd12
Alexey <line6@mail.ru>
2022-07-04 11:17:27 +0300

    Initial Commit

---
+++ ListAPI(testAssingment)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListAPI(testAssingment)
+//
+//  Created by Alexey on 7/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
